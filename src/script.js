62542c449e33352165ff511498ec75a7e73eea6c
Gustavo Mazeto Pasquini <gustavopasquini2006@hotmail.com>
2024-08-13 17:30:54 -0300

    Break the app on purpose

--- src/script.js
+++ src/script.js
@@ -32,7 +32,7 @@ document.querySelector('button[type="submit"]')
     if (!firstNameField.value || !lastNameField.value || !emailField.value || !textareaField.value) {
       return showAndHideErrorMessage()
     }
-    if (isPhoneRequired && !phoneField.value) {
+    if (isPhoneRequired && /*! erro aqui, apenas deixe o !*/phoneField.value) {
       return showAndHideErrorMessage()
     }
     if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
