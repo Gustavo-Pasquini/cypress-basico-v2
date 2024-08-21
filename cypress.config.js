48093753e936d20e5e56f91fcdf35709a5b1d800
Gustavo Mazeto Pasquini <gustavopasquini2006@hotmail.com>
2024-08-21 15:40:40 -0300

    Fixing Errors

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-=======
-module.exports = {
->>>>>>> 978d2128b8c27eb22e9c6a7a2c5e554a2fc287e6
+
   viewportWidth: 1280,
   viewportHeight: 880,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+    },
   },
-<<<<<<< HEAD
-})
-=======
-}
->>>>>>> 978d2128b8c27eb22e9c6a7a2c5e554a2fc287e6
+});
